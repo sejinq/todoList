b3ae3f2a47e6baf7d044675628711f8110cce931
se-jin son <thstpwlsq@gmail.com>
2017-05-18 02:43:25 +0900

    ui composition

--- todo-list.js
+++ todo-list.js
@@ -4,27 +4,45 @@ $('.ui.inline.dropdown')
   .dropdown({
     allowAdditions: true
 });
+
+var item = $('#items');
+var myNote = new Array();
+
 function note(title, content){
   this.title = title;
   this.content = content;
 }
-var myNote = {};
 
-//
-// $("#programmetype").dropdown({
-//             onChange: function (val) {
-//                 if (val == "val1")
-//                 {
-//                     $('#servicetype .menu').html(
-//                         '<div class="item" data-value="val1">Saloon</div>' +
-//                         '<div class="item" data-value="val2">Truck</div>'
-//                         );
-//                 };
-//
+//배열의 title을 받아 item을 추가 시켜줄 수 있는 html 코드로 변환해주는 함수
+function itemMsg(title){
+  var msg = $('<div class="item">'+title+'<div class="ui rating" data-max-rating="1"></div></div>');
+  return msg;
+};
+//드롭다운의 아이템을 배열의 값에 따라 업데이트 시켜준다.
+function updateItem(){
+  var initmsg = $('<div class="item" data-value="create">create todo<i class="plus icon"></i>');
+  for(var i in myNote){
+    item.html(initmsg.html());
+    item.append(itemMsg(myNote[i].title).html());
+  }
+}
+function create(title, content) {
+  var newNote = note(title, content);
+  myNote.push(newNote);
+  updateItem();
+};
+
+$('#update-btn').click(function() {
+  var title = $('#title').val();
+  var content = $('#content').val();
+  alert(title+content); //여기까지됨..
+  create(title, content);
+});
 $('#Menu').dropdown({
   onChange: function (val) {
     if(val == "create"){
-      alert('???');
+      $('#default-text').hide();
+      $('#todo-list-content').show();
     }
   }
 });
