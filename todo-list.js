37bbc8c83921bde85af6c6074f80288c79207e3f
se-jin son <thstpwlsq@gmail.com>
2017-05-25 02:29:36 +0900

    create function complete

--- todo-list.js
+++ todo-list.js
@@ -6,6 +6,7 @@ $('.ui.inline.dropdown')
 });
 
 var item = $('#items');
+var tempItem;
 var myNote = new Array();
 
 function note(title, content){
@@ -14,35 +15,87 @@ function note(title, content){
 }
 
 //배열의 title을 받아 item을 추가 시켜줄 수 있는 html 코드로 변환해주는 함수
-function itemMsg(title){
-  var msg = $('<div class="item">'+title+'<div class="ui rating" data-max-rating="1"></div></div>');
+function itemMsg(i, title){
+  var msg =$('<div><div class="item" data-value='+i+'>'+title+'<div class="ui rating" data-max-rating="1"></div></div>');
   return msg;
 };
 //드롭다운의 아이템을 배열의 값에 따라 업데이트 시켜준다.
 function updateItem(){
-  var initmsg = $('<div class="item" data-value="create">create todo<i class="plus icon"></i>');
-  for(var i in myNote){
-    item.html(initmsg.html());
-    item.append(itemMsg(myNote[i].title).html());
+  var initmsg = $('<div><div class="item" data-value="create">create todo<i class="plus icon"></i></div></div>');
+  item.html(initmsg.html());
+  for(var i=0;i<myNote.length;i++){
+    console.log(itemMsg(i, myNote[i].title).html());
+    item.append(itemMsg(i, myNote[i].title).html());
   }
 }
-function create(title, content) {
-  var newNote = note(title, content);
+function createItem(title, content) {
+  var newNote = new note(title, content);
   myNote.push(newNote);
   updateItem();
 };
+function editItem(title, content){
+  myNote[tempItem] = new note(title, content);
+  updateItem();
+};
+function deleteItem(){
+  myNote.splice(tempItem, 1);
+  updateItem();
+};
 
+$('#delete-btn').click(function(){
+  deleteItem();
+  goHome();
+})
 $('#update-btn').click(function() {
-  var title = $('#title').val();
-  var content = $('#content').val();
-  alert(title+content); //여기까지됨..
-  create(title, content);
+  var mode = $(this).val();
+  if( mode == 'Save'){
+    var title = $('#title').val();
+    var content = $('#content').val();
+    createItem(title, content);
+    alert("ok");
+    $('#title').val("");
+    $('#content').val("");
+  }
+  else if(mode == 'Update'){
+    var title = $('#title').val();
+    var content = $('#content').val();
+    editItem(title, content);
+    $('#update-btn').val("Save");
+  }
+  else if(mode == 'Edit'){
+    $('#title').attr("readonly",false);
+    $('#content').attr("readonly",false);
+    $('#update-btn').html("Save");
+    $('#update-btn').val("Update");
+  }
+
 });
+
+function goHome(){
+  $('#default-text').show();
+  $('#todo-list-content').hide();
+};
+
 $('#Menu').dropdown({
   onChange: function (val) {
     if(val == "create"){
       $('#default-text').hide();
+      $("#delete-btn").hide();
+      $('#update-btn').html("Save");
+      $('#update-btn').val("Save");
       $('#todo-list-content').show();
+    }
+    else{
+      $('#todo-list-content').show();
+      $("#delete-btn").show();
+      $('#update-btn').html("Edit");
+      $('#update-btn').val("Edit");
+
+      tempItem = Number(val);
+      $('#title').val(myNote[tempItem].title);
+      $('#content').val(myNote[tempItem].content);
+      $('#title').attr("readonly",true);
+      $('#content').attr("readonly",true);
     }
   }
 });
